c704d77f360b6f96db74a193c05c4216f930659d
Kaustubh Mowade <93318932+kaustubh1802@users.noreply.github.com>
2022-07-28 11:07:41 +0530

    Add files via upload

---
+++ B_Relatively_Prime_Pairs.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ long long l, r;
+ cin >> l >> r;
+ cout << "YES" << endl;
+ for (long long i = l; i < r + 1; i += 2)
+ {
+  cout << i << " " << i + 1 << endl;
+ }
+ return 0;
+}
